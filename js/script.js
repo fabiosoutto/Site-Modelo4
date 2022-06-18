6547dc0d43c18c1e2ff087dbe07c807aedd6ff0e
Fabio Soutto <fgimenes905@gmail.com>
2022-06-18 09:56:35 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.onscroll = function(){
+    scroll();
+};
+
+function scroll(){
+    let btn = document.getElementById("btntop");
+    if (document.documentElement.scrollTop > 50) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+}
+
+function backToTop() {
+    document.documentElement.scrollTop = 0;
+}
